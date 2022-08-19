15ab652292b073868bd3a123e039971a1194ecd7
piotrostr <piotr.ostrowski@aol.com>
2022-08-19 19:19:11 +0100

    init: set up styled components

---
+++ frontend/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const HeaderContainer = styled.div`
+  font-family: 'Ubuntu', sans-serif;
+  font-size: 10rem;
+`;
+
+export default function Header() {
+  return <HeaderContainer>hello</HeaderContainer>;
+}
