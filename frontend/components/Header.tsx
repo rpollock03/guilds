040a0056e9dbb6bcb6ec44ccf81db33d55beea44
piotrostr <piotr.ostrowski@aol.com>
2022-08-19 19:42:36 +0100

    init: eslint

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -3,9 +3,19 @@ import styled from 'styled-components';
 
 const HeaderContainer = styled.div`
   font-family: 'Ubuntu', sans-serif;
-  font-size: 10rem;
+  font-size: 5rem;
 `;
 
 export default function Header() {
-  return <HeaderContainer>hello</HeaderContainer>;
+  const tmp = [1, 2, 3];
+  return (
+    <HeaderContainer>
+      hello
+      <div>
+        {tmp.map((item) => (
+          <div>{item}</div>
+        ))}
+      </div>
+    </HeaderContainer>
+  );
 }
