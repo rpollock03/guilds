a66250641016c3c0edb1e216ee2f87e0f338e848
piotrostr <piotr.ostrowski@aol.com>
2022-08-19 20:37:26 +0100

    style: run prettier

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,10 +1,10 @@
-import Head from 'next/head';
-import React from 'react';
-import styles from '../styles/Home.module.css';
+import Head from "next/head";
+import React from "react";
+import styles from "../styles/Home.module.css";
 
-import Header from '../components/Header';
+import Header from "../components/Header";
 
-export default function Home({}) {
+export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
