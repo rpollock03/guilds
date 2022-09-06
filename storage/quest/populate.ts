8c3226855a8964a26ad2ec81555ae03bcc70fd1f
Oliwier Ostrowski <oliwier.ostro@gmail.com>
2022-09-06 16:53:41 +0200

    add populateBids function to quest storage

--- storage/quest/populate.ts
+++ storage/quest/populate.ts
@@ -1,5 +1,12 @@
-import { Firestore, collection, getDocs, addDoc } from "firebase/firestore"
-import { Quest } from "../../storage/quest"
+import {
+  Firestore,
+  collection,
+  getDocs,
+  addDoc,
+  QueryDocumentSnapshot,
+  DocumentData,
+} from "firebase/firestore"
+import { Quest, Bid } from "../../storage/quest"
 import { faker } from "@faker-js/faker"
 
 export const populateQuests = async (firestore: Firestore) => {
@@ -17,4 +24,26 @@ export const populateQuests = async (firestore: Firestore) => {
     const results = await Promise.all(promises)
     console.log(results[0])
   }
+}
+
+export const populateBids = async (firestore: Firestore) => {
+  const questsRef = collection(firestore, `quests`)
+  const quests = await getDocs(questsRef)
+  const promises = []
+  quests.docs.forEach((quest: QueryDocumentSnapshot<DocumentData>) => {
+    const questId = quest.id
+    const questRef = collection(firestore, `quests/${questId}/bids`)
+    for (let i = 0; i < 3; i++) {
+      const bid: Bid = {
+        bidId: faker.datatype.uuid(),
+        questId: questId,
+        userId: faker.datatype.uuid(),
+        amount: faker.datatype.number({ min: 1, max: 1000 }),
+        timeEstimate: `${faker.datatype.number({ min: 1, max: 100 })} days`,
+        createdAt: faker.date.past(),
+      }
+      promises.push(addDoc(questRef, bid))
+    }
+  })
+  const results = await Promise.all(promises)
 }
