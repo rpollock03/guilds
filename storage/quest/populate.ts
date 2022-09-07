9a348c48debcc5296eebe889ae38c1ff1a06f206
Oliwier Ostrowski <oliwier.ostro@gmail.com>
2022-09-07 15:55:08 +0200

    add function for populating bids

--- storage/quest/populate.ts
+++ storage/quest/populate.ts
@@ -5,24 +5,27 @@ import {
   addDoc,
   QueryDocumentSnapshot,
   DocumentData,
+  doc,
+  setDoc,
 } from "firebase/firestore"
-import { Quest, Bid } from "../../storage/quest"
+import { Quest, Bid, Tag } from "../../storage/quest"
 import { faker } from "@faker-js/faker"
 
 export const populateQuests = async (firestore: Firestore) => {
-  const questRef = collection(firestore, `quests`)
   const promises = []
   for (let i = 0; i < 3; i++) {
+    const questId = faker.datatype.uuid()
+    const questRef = doc(firestore, `quests`, questId)
     const quest: Quest = {
+      id: questId,
       image: faker.image.imageUrl(),
       reward: faker.datatype.number({ min: 1, max: 1000 }),
       title: faker.lorem.sentence(5),
       description: faker.lorem.sentences(5),
-      tags: [],
+      tags: [Object.values(Tag)[faker.datatype.number(6)]],
     }
-    promises.push(addDoc(questRef, quest))
+    promises.push(setDoc(questRef, quest))
     const results = await Promise.all(promises)
-    console.log(results[0])
   }
 }
 
@@ -31,18 +34,18 @@ export const populateBids = async (firestore: Firestore) => {
   const quests = await getDocs(questsRef)
   const promises = []
   quests.docs.forEach((quest: QueryDocumentSnapshot<DocumentData>) => {
-    const questId = quest.id
-    const questRef = collection(firestore, `quests/${questId}/bids`)
     for (let i = 0; i < 3; i++) {
+      const bidId = faker.datatype.uuid()
+      const questRef = doc(firestore, `quests/${quest.id}/bids`, bidId)
       const bid: Bid = {
-        bidId: faker.datatype.uuid(),
-        questId: questId,
+        bidId: bidId,
+        questId: quest.id,
         userId: faker.datatype.uuid(),
         amount: faker.datatype.number({ min: 1, max: 1000 }),
         timeEstimate: `${faker.datatype.number({ min: 1, max: 100 })} days`,
         createdAt: faker.date.past(),
       }
-      promises.push(addDoc(questRef, bid))
+      promises.push(setDoc(questRef, bid))
     }
   })
   const results = await Promise.all(promises)
