aecff835c0c84bdc2d129ce89959736872105bd9
Oliwier Ostrowski <oliwier.ostro@gmail.com>
2022-09-09 15:37:45 +0200

    add components for searching for quests

--- components/Quests.tsx
+++ components/Quests.tsx
@@ -2,14 +2,27 @@ import { Grid } from "styled-css-grid"
 import { useFirestore, useFirestoreCollectionData } from "reactfire"
 import { collection, query } from "firebase/firestore"
 import { populateQuests } from "../storage/quest"
+import { InstantSearch, SearchBox, Hits, Stats } from "react-instantsearch-dom"
+import { searchClient } from "../typesense/insantsearch"
 
 export default function Quests(): JSX.Element {
   const firestore = useFirestore()
   const questsQuery = query(collection(firestore, "quests"))
   const { status, data: quests } = useFirestoreCollectionData(questsQuery)
 
+  const Hit = ({ hit }) => (
+    <div>
+      <div>Title: {hit?.title}</div>
+      <div>Description: {hit?.description}</div>
+      <div>Reward: {hit?.reward}</div>
+      <div>Tags: {hit?.tags[0]}</div>
+    </div>
+  )
+
   return (
-    <>
+    <InstantSearch searchClient={searchClient} indexName="quests">
+      <SearchBox />
+      <Stats />
       <button
         onClick={() => populateQuests(firestore)}
         style={{ color: "black" }}
@@ -22,22 +35,11 @@ export default function Quests(): JSX.Element {
             <div>loading</div>
           ) : (
             <Grid columns={"repeat(auto-fit, minmax(210px, 1fr))"} gap={"83px"}>
-              {quests?.length ? (
-                quests.map((quest, idx) => (
-                  <div key={idx}>
-                    <div>{quest?.title}</div>
-                    <div>{quest?.description}</div>
-                    <div>{quest?.reward}</div>
-                    <div>{quest?.tags}</div>
-                  </div>
-                ))
-              ) : (
-                <div style={{ margin: "auto" }}>no quests</div>
-              )}
+              <Hits hitComponent={Hit} />
             </Grid>
           )}
         </>
       )}
-    </>
+    </InstantSearch>
   )
 }
