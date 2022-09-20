aa4552156f17501fff1731f3fa410ee2ca281da9
Oliwier Ostrowski <oliwier.ostro@gmail.com>
2022-09-20 16:57:32 +0200

    add teams, quests, stats, bounty basic pages and components

---
+++ pages/quests.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components"
+import styles from "../styles/Home.module.css"
+import Quests from "components/Quests"
+
+const Title = styled.div`
+  font-size: 100px;
+  margin-bottom: 100px;
+`
+
+export default function quests() {
+  return (
+    <div className={styles.container}>
+      <Title>Quests</Title>
+    </div>
+  )
+}
