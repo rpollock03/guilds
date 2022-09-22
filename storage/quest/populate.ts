4ef0a431cdb721a12eb87d7ff4f8ac4e6af10649
Oliwier Ostrowski <oliwier.ostro@gmail.com>
2022-09-22 15:37:17 +0200

    change the populated data to make testing easier

--- storage/quest/populate.ts
+++ storage/quest/populate.ts
@@ -13,23 +13,27 @@ import { faker } from "@faker-js/faker"
 const populateQuests = async (firestore: Firestore) => {
   try {
     const promises = []
-    const developerId = [
-      "imZrHGqnOCfGy44ya596jOyNHIG3",
-      "QfABV59rDVWcUDBvtiaZCrQ8mTJ2",
-      "zfUtzkbpCdRVjBKHACyT5pjg1bb2",
-    ][faker.datatype.number({ min: 0, max: 2 })]
     for (let i = 0; i < 3; i++) {
+      const developerIds = [
+        "imZrHGqnOCfGy44ya596jOyNHIG3",
+        "QfABV59rDVWcUDBvtiaZCrQ8mTJ2",
+        "zfUtzkbpCdRVjBKHACyT5pjg1bb2",
+      ]
+      const userId = developerIds[faker.datatype.number({ min: 0, max: 2 })]
+      const bidders = developerIds.filter((id) => id !== userId)
+      console.log("usesId", userId)
+      console.log("bidders", bidders)
       const questsRef = collection(firestore, "quests")
       const questRef = doc(questsRef)
       const quest: Quest = {
         questId: questRef.id,
-        userId: developerId,
+        userId: userId,
         image: faker.image.imageUrl(),
         reward: faker.datatype.number({ min: 1, max: 1000 }),
         title: faker.lorem.sentence(5),
         description: faker.lorem.sentences(5),
         tags: [Object.values(Tag)[faker.datatype.number(6)]],
-        bidders: [developerId],
+        bidders: bidders,
         status: ["open", "closed"][faker.datatype.number(1)],
       }
       promises.push(setDoc(questRef, quest))
@@ -53,8 +57,9 @@ const populateBids = async (firestore: Firestore) => {
         const bidRef = doc(bidsRef)
         const bid: Bid = {
           bidId: bidRef.id,
-          questId: quest.id,
-          userId: faker.datatype.uuid(),
+          questId: quest.data().questId,
+          userId:
+            quest.data().bidders[faker.datatype.number({ min: 0, max: 1 })],
           amount: faker.datatype.number({ min: 1, max: 1000 }),
           timeEstimate: `${faker.datatype.number({ min: 1, max: 100 })} days`,
           createdAt: faker.date.past(),
