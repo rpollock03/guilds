9b67e3308b5995754944272b7c01044e56d9d406
Oliwier Ostrowski <oliwier.ostro@gmail.com>
2022-09-23 21:45:22 +0200

    refactor: rename add-bid, add-quest, and freelancer-hub pages nad fix addBidPage name

--- pages/add-bid.tsx
+++ pages/add-bid.tsx
@@ -1,7 +1,7 @@
 import styles from "../styles/Home.module.css"
 import AddBid from "../components/AddBid"
 
-export default function AddQuestPage(): JSX.Element {
+export default function AddBidPage(): JSX.Element {
   return (
     <div className={styles.container}>
       <AddBid />
