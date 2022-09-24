9f8e042903587af56ecb47f2af85c791ddf428bc
piotrostr <piotr.ostrowski@aol.com>
2022-09-24 20:22:00 +0100

    fix warning: Do not add stylesheets using next/head, fix theme, sync with system settings

--- theme/index.ts
+++ theme/index.ts
@@ -1,28 +1,30 @@
-import { createTheme } from "@mui/material/styles"
+import { ThemeOptions } from '@mui/material/styles';
 
-export const lightTheme = createTheme({
+export const lightThemeOptions: ThemeOptions = {
   palette: {
+    mode: 'light',
     primary: {
-      main: "#41764A",
-      light: "#D8E9DA",
+      main: '#41764A',
+      light: '#D8E9DA',
     },
     secondary: {
-      main: "#101828",
-      light: "#1D2939",
+      main: '#101828',
+      light: '#1D2939',
     },
     background: {
-      default: "#FFFFFF",
-      paper: "#F9FAFB",
+      default: '#FFFFFF',
+      paper: '#F9FAFB',
     },
     text: {
-      primary: "#101828",
-      secondary: "#667085",
+      primary: '#101828',
+      secondary: '#667085',
     },
   },
-})
+}
 
-export const darkTheme = createTheme({
+export const darkThemeOptions: ThemeOptions = {
   palette: {
+    mode: "dark",
     primary: {
       main: "#41764A",
       light: "#D8E9DA",
@@ -40,4 +42,4 @@ export const darkTheme = createTheme({
       secondary: "#EAECF0",
     },
   },
-})
+}
