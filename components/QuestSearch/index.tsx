39555b52827b053afc87bcdd3a4c037721e29182
Oliwier Ostrowski <oliwier.ostro@gmail.com>
2022-10-23 16:03:28 +0200

    Refactor the quest search assebly component

---
+++ components/QuestSearch/index.tsx
@@ -0,0 +1,38 @@
+import { QuestHits } from "./QuestHits"
+import { useFirestore } from "reactfire"
+import { populateQuests, populateBids } from "storage/quest"
+import { searchClient } from "typesense/insantsearch"
+import { InstantSearch } from "react-instantsearch-dom"
+import { QuestRefinementList } from "./QuestRefinementList"
+import { Button, Stack, Divider } from "@mui/material"
+import { QuestSearchBox } from "./QuestSearchBox"
+import { QuestPagination } from "./QuestPagination"
+
+export function Quests(): JSX.Element {
+  const firestore = useFirestore()
+
+  return (
+    <InstantSearch searchClient={searchClient} indexName="quests">
+      <Stack direction="column" justifyContent="space-around" spacing={4}>
+        <Stack direction="row" spacing={2}>
+          <Stack direction="column" spacing={4}>
+            <QuestSearchBox />
+            <QuestRefinementList attribute="tags" />
+            <Button
+              variant="outlined"
+              onClick={() => populateQuests(firestore)}
+            >
+              populate quests
+            </Button>
+            <Button variant="outlined" onClick={() => populateBids(firestore)}>
+              populate bids
+            </Button>
+          </Stack>
+          <QuestHits />
+        </Stack>
+        <Divider />
+        <QuestPagination />
+      </Stack>
+    </InstantSearch>
+  )
+}
