b535b3a5545628780095978529af175e390c88ab
Oliwier Ostrowski <oliwier.ostro@gmail.com>
2022-10-23 15:25:53 +0200

    Refactor the quest search box component

---
+++ components/QuestSearch/QuestSearchBox.tsx
@@ -0,0 +1,35 @@
+import { connectSearchBox } from "react-instantsearch-dom"
+import { Box, InputAdornment, TextField } from "@mui/material"
+import Image from "next/image"
+import styled from "styled-components"
+
+const CustomTextField = styled(TextField)`
+  .MuiOutlinedInput-root {
+    border-radius: 0.5rem;
+  }
+`
+
+function SearchBox({ currentRefinement, refine }) {
+  return (
+    <Box>
+      <CustomTextField
+        size="small"
+        variant="outlined"
+        type="search"
+        value={currentRefinement}
+        onChange={(event) => refine(event.currentTarget.value)}
+        placeholder="Search"
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <Image src={"/search.svg"} width={20} height={20} />
+            </InputAdornment>
+          ),
+        }}
+        sx={{ width: "18rem", borderRadius: "1rem" }}
+      />
+    </Box>
+  )
+}
+
+export const QuestSearchBox = connectSearchBox(SearchBox)
