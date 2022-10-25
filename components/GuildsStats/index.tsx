afb9cd755918dc9da5d9a9fa488ebabcdc2c790b
Jakub Frackowiak <xfragless@gmail.com>
2022-10-25 20:40:51 +0200

    add Earned by heroes section

--- components/GuildsStats/index.tsx
+++ components/GuildsStats/index.tsx
@@ -1,7 +1,8 @@
-import { Box, Stack, Typography } from "@mui/material"
-import { collection, where, query } from "firebase/firestore"
-import { useFirestore, useFirestoreCollectionData, useUser } from "reactfire"
 import Image from "next/image"
+import { Divider, Stack, Typography } from "@mui/material"
+import { collection, where, query } from "firebase/firestore"
+import { useFirestore, useFirestoreCollectionData } from "reactfire"
+import { Container } from "@mui/system"
 
 export function index() {
   const firestore = useFirestore()
@@ -10,31 +11,66 @@ export function index() {
   const questsRef = collection(firestore, "quests")
   const closedQuestsQuery = query(questsRef, where("status", "==", "closed"))
   const { data: closedQuests } = useFirestoreCollectionData(closedQuestsQuery)
+  const totalEarned = closedQuests
+    .map((quest) => quest.reward)
+    .reduce((a, b) => a + b, 0)
 
   return (
-    <Stack alignItems="center" justifyContent="space-around" spacing={2}>
-      <Image src="/GuildsLogo.svg" width={56} height={56} />
-      <Typography variant="h4">Guilds history</Typography>
-      <Typography variant="h6" sx={{ color: "text.secondary" }}>
-        We've made quite the statement
-      </Typography>
-      <Stack direction="row" justifyContent="space-around" spacing={2}>
-        <Stack alignItems="center">
-          <Typography variant="h4" sx={{ color: "primary.main" }}>
-            {heroes.length}
-          </Typography>
-          <Typography variant="h6">Heroes</Typography>
-        </Stack>
-        <Stack alignItems="center">
-          <Typography variant="h6">Earned by Heroes</Typography>
-        </Stack>
-        <Stack alignItems="center">
-          <Typography variant="h4" sx={{ color: "primary.main" }}>
-            {closedQuests.length}
-          </Typography>
-          <Typography variant="h6">Quests completed</Typography>
-        </Stack>
+    <Stack
+      alignItems="center"
+      justifyContent="space-around"
+      spacing={7}
+      width="100%"
+    >
+      <Stack alignItems="center" spacing={2}>
+        <Image src="/GuildsLogo.svg" width={56} height={56} />
+        <Typography variant="h4">Guilds history</Typography>
+        <Typography variant="h6" sx={{ color: "text.secondary" }}>
+          We've made quite the statement
+        </Typography>
       </Stack>
+      <Container maxWidth="md">
+        <Stack direction="row" justifyContent="space-around" width="100%">
+          <Stack spacing={1} alignItems="center" width="calc(100%/3)">
+            <Typography
+              fontWeight={600}
+              variant="h3"
+              sx={{ color: "primary.main" }}
+            >
+              {heroes?.length.toLocaleString()}
+            </Typography>
+            <Typography textAlign="center" variant="body1">
+              Heroes
+            </Typography>
+          </Stack>
+          <Divider orientation="vertical" flexItem />
+          <Stack spacing={1} alignItems="center" width="calc(100%/3)">
+            <Typography
+              fontWeight={600}
+              variant="h3"
+              sx={{ color: "primary.main" }}
+            >
+              £{totalEarned.toLocaleString()}
+            </Typography>
+            <Typography textAlign="center" variant="body1">
+              Earned by Heroes
+            </Typography>
+          </Stack>
+          <Divider orientation="vertical" flexItem />
+          <Stack spacing={1} alignItems="center" width="calc(100%/3)">
+            <Typography
+              fontWeight={600}
+              variant="h3"
+              sx={{ color: "primary.main" }}
+            >
+              {closedQuests?.length.toLocaleString()}
+            </Typography>
+            <Typography textAlign="center" variant="body1">
+              Quests completed
+            </Typography>
+          </Stack>
+        </Stack>
+      </Container>
     </Stack>
   )
 }
