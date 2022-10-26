53cd6406c2be42315ac62d8326f3488c8961cbd2
Oliwier Ostrowski <oliwier.ostro@gmail.com>
2022-10-26 13:04:37 +0200

    Add populate members function

--- storage/team/populate.tsx
+++ storage/team/populate.tsx
@@ -13,19 +13,19 @@ import { Team, Role, Industry } from "../../storage/team"
 const populateTeams = async (firestore: Firestore) => {
   try {
     const promises = []
-    const heroesRef = collection(firestore, `heroes`)
-    const heroes = await getDocs(heroesRef)
     for (let i = 0; i < 3; i++) {
-      const roles = Object.values(Role).filter(() => Math.random() > 0.5)
+      const roles = Object.values(Role)
+      const selectedRoles = []
+      for (let i = 0; i < faker.datatype.number({ min: 2, max: 10 }); i++) {
+        selectedRoles.push(
+          roles[faker.datatype.number({ min: 0, max: roles.length - 1 })]
+        )
+      }
       const developerIds = [
         "imZrHGqnOCfGy44ya596jOyNHIG3",
         "QfABV59rDVWcUDBvtiaZCrQ8mTJ2",
         "zfUtzkbpCdRVjBKHACyT5pjg1bb2",
       ]
-      const slots = faker.datatype.number({ min: 1, max: 10 })
-      const heroesIds = heroes?.docs
-        ?.map((hero: QueryDocumentSnapshot<DocumentData>) => hero.id)
-        .splice(0, faker.datatype.number({ min: 1, max: slots }))
       const userId = developerIds[faker.datatype.number({ min: 0, max: 2 })]
       const teamsRef = collection(firestore, "teams")
       const teamRef = doc(teamsRef)
@@ -34,13 +34,11 @@ const populateTeams = async (firestore: Firestore) => {
         creatorId: userId,
         title: faker.lorem.sentence(5),
         description: faker.lorem.sentences(5),
-        roles: roles,
+        roles: selectedRoles,
         industry:
           Object.values(Industry)[faker.datatype.number({ min: 0, max: 3 })],
         image: "office.jpeg",
         timeEstimate: faker.datatype.number({ min: 1, max: 10 }) + " months",
-        slots: slots,
-        members: heroesIds,
         createdAt: faker.date.past(),
       }
       promises.push(setDoc(teamRef, team))
@@ -53,4 +51,41 @@ const populateTeams = async (firestore: Firestore) => {
   }
 }
 
-export { populateTeams }
+const populateMembers = async (firestore: Firestore) => {
+  try {
+    const promises = []
+    const heroesRef = collection(firestore, `heroes`)
+    const heroesSnap = await getDocs(heroesRef)
+    const teamsRef = collection(firestore, `teams`)
+    const teamsSnap = await getDocs(teamsRef)
+    console.log("teamsSnap", teamsSnap.docs)
+    teamsSnap?.docs.forEach(async (team) => {
+      const slots = team?.data().roles.length
+      console.log("slots", slots)
+      const members = heroesSnap?.docs?.splice(
+        0,
+        faker.datatype.number({ min: 1, max: slots })
+      )
+      const membersRef = collection(firestore, `teams/${team.id}/members`)
+      const membersSnap = await getDocs(membersRef)
+      if (membersSnap?.docs?.length === 0) {
+        members?.forEach((member) => {
+          console.log("memberdata", member.data())
+          promises.push(
+            setDoc(
+              doc(firestore, `teams/${team.id}/members`, member.id),
+              member.data()
+            )
+          )
+        })
+      }
+    })
+    const results = await Promise.all(promises)
+    alert("Members created")
+  } catch (e) {
+    console.error(e)
+    alert("Error: " + e)
+  }
+}
+
+export { populateTeams, populateMembers }
