63294b11db0c38d998b7e8a2330e75f55b1d9dfe
Oliwier Ostrowski <oliwier.ostro@gmail.com>
2022-10-27 17:36:54 +0200

    Add chip highlight to latest team component

--- components/LatestTeamsSlider/LatestTeam.tsx
+++ components/LatestTeamsSlider/LatestTeam.tsx
@@ -2,14 +2,15 @@ import Link from "next/link"
 import styled from "styled-components"
 import Image from "next/image"
 import LinesElipsis from "react-lines-ellipsis"
-import { Grid, Box, Stack, Typography } from "@mui/material"
+import { Grid, Chip, Stack, Typography } from "@mui/material"
 import { Team } from "storage/team"
 import { StorageImage } from "reactfire"
-import { TeamMember } from "./TeamMember"
+import { TeamMembers } from "./TeamMembers"
 
-const QuestThumbnail = styled(StorageImage)({
+const TeamThumbnail = styled(StorageImage)({
   objectFit: "cover",
   height: 240,
+  width: 420,
 })
 
 interface LatestTeamProps {
@@ -17,56 +18,47 @@ interface LatestTeamProps {
 }
 
 export function LatestTeam({ team }: LatestTeamProps) {
-  const members = new Array(team.slots)
-    .fill(0)
-    .map((_, idx) =>
-      team.members[idx] != undefined ? team.members[idx] : "empty slot"
-    )
-
-  const convertImageName = () => {
-    const [imageName, imageExtension] = team.image.split(".")
-    return imageName + "_420x240." + imageExtension
-  }
-
   return (
     <Grid item xs={6}>
-      <Box>
-        <Stack spacing={3}>
-          <QuestThumbnail
-            storagePath={`quests/questsResized/${convertImageName()}`}
-          />
-          <Stack direction="row" justifyContent="space-between">
-            <Typography variant="h6">{team?.title}</Typography>
-            <Link
-              href={{
-                pathname: "/team",
-                query: { teamId: team.id },
-              }}
-            >
-              <Image
-                src="/arrow-right-up.svg"
-                alt="arrow-right-up"
-                width={15}
-                height={15}
-              />
-            </Link>
-          </Stack>
-          <Typography variant="body1">
-            <LinesElipsis
-              text={team.description}
-              maxLine="2"
-              ellipsis="..."
-              trimRight
-              basedOn="words"
+      <Stack spacing={3}>
+        <TeamThumbnail storagePath={`teams/${team.image}`} />
+        <Stack direction="row" justifyContent="space-between">
+          <Typography variant="h6">{team?.title}</Typography>
+          <Link
+            href={{
+              pathname: "/team",
+              query: { teamId: team.id },
+            }}
+          >
+            <Image
+              src="/arrow-right-up.svg"
+              alt="arrow-right-up"
+              width={15}
+              height={15}
             />
-          </Typography>
-          <Stack direction="row" spacing={1}>
-            {members.map((member, idx) => (
-              <TeamMember key={idx} member={member} />
-            ))}
-          </Stack>
+          </Link>
         </Stack>
-      </Box>
+        <Typography variant="body1">
+          <LinesElipsis
+            text={team.description}
+            maxLine="2"
+            ellipsis="..."
+            trimRight
+            basedOn="words"
+          />
+        </Typography>
+        <Stack direction="row" spacing={1}>
+          <TeamMembers team={team} />
+        </Stack>
+        <Chip
+          label={team.highlight}
+          sx={{
+            width: "fit-content",
+            backgroundColor: "primary.light",
+            color: "primary.main",
+          }}
+        />
+      </Stack>
     </Grid>
   )
 }
