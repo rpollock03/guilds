0b508dd88e37345937d614780c123ccfdbbc541c
Oliwier Ostrowski <oliwier.ostro@gmail.com>
2022-10-29 14:39:15 +0200

    add team search component

---
+++ components/TeamSearch/index.tsx
@@ -0,0 +1,51 @@
+import { TeamHits } from "./TeamHits"
+import { useFirestore } from "reactfire"
+import { searchClient } from "typesense/insantsearch"
+import { InstantSearch } from "react-instantsearch-dom"
+import { RefinementList, Pagination, SearchBox } from "../SearchComponents"
+import { Button, Stack, Divider, Container } from "@mui/material"
+import { populateMembers, populateTeams } from "storage/team"
+import { FindTeamBanner } from "./FindTeamBanner"
+
+export function Teams(): JSX.Element {
+  const firestore = useFirestore()
+
+  return (
+    <Stack>
+      <FindTeamBanner />
+      <Container>
+        <InstantSearch searchClient={searchClient} indexName="teams">
+          <Stack
+            direction="column"
+            justifyContent="space-around"
+            spacing={4}
+            mb="4rem"
+          >
+            <Stack direction="row" spacing={6}>
+              <Stack direction="column" spacing={4}>
+                <SearchBox />
+                <RefinementList attribute="roles" label={"Role categories"} />
+                <RefinementList attribute="industry" label={"Industry"} />
+                <Button
+                  variant="outlined"
+                  onClick={() => populateTeams(firestore)}
+                >
+                  populate teams
+                </Button>
+                <Button
+                  variant="outlined"
+                  onClick={() => populateMembers(firestore)}
+                >
+                  populate memabers
+                </Button>
+              </Stack>
+              <TeamHits />
+            </Stack>
+            <Divider />
+            <Pagination />
+          </Stack>
+        </InstantSearch>
+      </Container>
+    </Stack>
+  )
+}
