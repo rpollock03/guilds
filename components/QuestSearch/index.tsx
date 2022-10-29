46013b73d8f9e6617eb83133e8d48ff36aeb59dd
Oliwier Ostrowski <oliwier.ostro@gmail.com>
2022-10-29 17:05:22 +0200

    adjust quest search to be consistant with teams search component

--- components/QuestSearch/index.tsx
+++ components/QuestSearch/index.tsx
@@ -1,41 +1,50 @@
 import { QuestHits } from "./QuestHits"
 import { useFirestore } from "reactfire"
 import { populateQuests, populateBids } from "storage/quest"
-import { searchClient } from "typesense/insantsearch"
+import { questsSearchClient } from "typesense/insantsearch"
 import { InstantSearch } from "react-instantsearch-dom"
 import { RefinementList, Pagination, SearchBox } from "../SearchComponents"
-import { Button, Stack, Divider } from "@mui/material"
+import { Button, Stack, Divider, Container } from "@mui/material"
+import { FindQuestBanner } from "./FindQuestBanner"
 
 export function Quests(): JSX.Element {
   const firestore = useFirestore()
 
   return (
-    <InstantSearch searchClient={searchClient} indexName="quests">
-      <Stack
-        direction="column"
-        justifyContent="space-around"
-        spacing={4}
-        mb="4rem"
-      >
-        <Stack direction="row" spacing={6}>
-          <Stack direction="column" spacing={4}>
-            <SearchBox />
-            <RefinementList attribute="tags" />
-            <Button
-              variant="outlined"
-              onClick={() => populateQuests(firestore)}
-            >
-              populate quests
-            </Button>
-            <Button variant="outlined" onClick={() => populateBids(firestore)}>
-              populate bids
-            </Button>
+    <Stack>
+      <FindQuestBanner />
+      <Container>
+        <InstantSearch searchClient={questsSearchClient} indexName="quests">
+          <Stack
+            direction="column"
+            justifyContent="space-around"
+            spacing={4}
+            mb="4rem"
+          >
+            <Stack direction="row" spacing={6}>
+              <Stack direction="column" spacing={4}>
+                <SearchBox />
+                <RefinementList attribute="tags" label="Quest categories" />
+                <Button
+                  variant="outlined"
+                  onClick={() => populateQuests(firestore)}
+                >
+                  populate quests
+                </Button>
+                <Button
+                  variant="outlined"
+                  onClick={() => populateBids(firestore)}
+                >
+                  populate bids
+                </Button>
+              </Stack>
+              <QuestHits />
+            </Stack>
+            <Divider />
+            <Pagination />
           </Stack>
-          <QuestHits />
-        </Stack>
-        <Divider />
-        <Pagination />
-      </Stack>
-    </InstantSearch>
+        </InstantSearch>
+      </Container>
+    </Stack>
   )
 }
