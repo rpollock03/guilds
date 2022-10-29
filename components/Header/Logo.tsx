72d0c98b282e5d582ae6a5ffaaf2736ab4f908b1
Piotr Ostrowski <piotrostr@google.com>
2022-10-29 23:36:56 +0200

    fix: add Image alts

--- components/Header/Logo.tsx
+++ components/Header/Logo.tsx
@@ -7,7 +7,7 @@ export const Logo = () => {
     <Link href="/">
       <a style={{ textDecoration: "none", color: "inherit" }}>
         <Box sx={{ display: "flex", alignItems: "center" }}>
-          <Image src="/guilds-logo.svg" width={36} height={36} />
+          <Image src="/guilds-logo.svg" width={36} height={36} alt="Guilds Logo" />
           <Typography variant="h6" sx={{ ml: 0.8, fontWeight: 700 }}>
             Guilds
           </Typography>
