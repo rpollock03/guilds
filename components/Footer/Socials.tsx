f5f8efb7280f90d57ea7ab444928f03d733c96e4
Oliwier Ostrowski <oliwier.ostro@gmail.com>
2022-11-02 01:30:21 +0100

    Add pointer cursor to socials icons

--- components/Footer/Socials.tsx
+++ components/Footer/Socials.tsx
@@ -5,27 +5,27 @@ import { Stack, Box } from "@mui/material"
 export function Socials() {
   return (
     <Stack direction="row" spacing={3} alignItems="center">
-      <Box>
+      <Box sx={{ cursor: "pointer" }}>
         <Link href="#">
           <Image src="/twitter.svg" alt="Twitter" width={24} height={24} />
         </Link>
       </Box>
-      <Box>
+      <Box sx={{ cursor: "pointer" }}>
         <Link href="#">
           <Image src="/linkedin.svg" alt="LinkedIn" width={24} height={24} />
         </Link>
       </Box>
-      <Box>
+      <Box sx={{ cursor: "pointer" }}>
         <Link href="#">
           <Image src="/facebook.svg" alt="Facebook" width={24} height={24} />
         </Link>
       </Box>
-      <Box>
+      <Box sx={{ cursor: "pointer" }}>
         <Link href="#">
           <Image src="/instagram.svg" alt="Instagram" width={24} height={24} />
         </Link>
       </Box>
-      <Box>
+      <Box sx={{ cursor: "pointer" }}>
         <Link href="#">
           <Image src="/youtube.svg" alt="Youtube" width={24} height={24} />
         </Link>
