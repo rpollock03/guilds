13fe82fe47ba27ebc62dd6ce20024c423f95df05
Piotr Ostrowski <piotrostr@google.com>
2022-11-03 16:23:31 +0000

    refactor: move FooterLogo component to a separate file

---
+++ components/Footer/FooterLogo.tsx
@@ -0,0 +1,40 @@
+import Image from "next/image"
+import { LinkColumns } from "./LinkColumns"
+import { Stack, Divider, Typography } from "@mui/material"
+
+export function FooterLogo() {
+  return (
+    <Stack>
+      <Stack direction="row" height="80%">
+        <Stack spacing={4} width="20rem">
+          <Stack direction="row" alignItems="center" spacing={1}>
+            <Image
+              src="/GuildsLogo2.svg"
+              alt="Guilds Logo"
+              width={32}
+              height={32}
+            />
+            <Typography
+              variant="h5"
+              sx={{ color: "background.default", fontWeight: 700 }}
+            >
+              Guilds
+            </Typography>
+          </Stack>
+          <Typography sx={{ color: "background.default" }}>
+            Design amazing digital experiences that create more happy in the
+            world
+          </Typography>
+        </Stack>
+        <LinkColumns />
+      </Stack>
+      <Divider
+        sx={{
+          bgcolor: (theme) =>
+            theme.palette.mode == "light" ? "text.secondary" : "primary.main",
+          mt: "5rem",
+        }}
+      />
+    </Stack>
+  )
+}
--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,17 +1,11 @@
-import Image from "next/image"
-import { Box, Typography, Container, Stack, Divider } from "@mui/material"
-import { LinkColumns } from "./LinkColumns"
+import { Box, Typography, Container, Stack } from "@mui/material"
 import { Subscribe } from "./Subscribe"
 import { Socials } from "./Socials"
+import { FooterLogo } from "./FooterLogo"
 
 export function Footer() {
   return (
-    <Box
-      component="footer"
-      sx={{
-        mt: "auto",
-      }}
-    >
+    <Box component="footer" sx={{ mt: "auto" }}>
       <Stack>
         <Box sx={{ bgcolor: "secondary.main" }}>
           <Container>
@@ -42,40 +36,7 @@ export function Footer() {
         </Box>
         <Box height="30rem" sx={{ bgcolor: "secondary.light" }}>
           <Container sx={{ mt: "3rem" }}>
-            <Stack>
-              <Stack direction="row" height="80%">
-                <Stack spacing={4} width="20rem">
-                  <Stack direction="row" alignItems="center" spacing={1}>
-                    <Image
-                      src="/GuildsLogo2.svg"
-                      alt="Guilds Logo"
-                      width={32}
-                      height={32}
-                    />
-                    <Typography
-                      variant="h5"
-                      sx={{ color: "background.default", fontWeight: 700 }}
-                    >
-                      Guilds
-                    </Typography>
-                  </Stack>
-                  <Typography sx={{ color: "background.default" }}>
-                    Design amazing digital experiences that create more happy in
-                    the world
-                  </Typography>
-                </Stack>
-                <LinkColumns />
-              </Stack>
-              <Divider
-                sx={{
-                  bgcolor: (theme) =>
-                    theme.palette.mode == "light"
-                      ? "text.secondary"
-                      : "primary.main",
-                  mt: "5rem",
-                }}
-              />
-            </Stack>
+            <FooterLogo />
             <Stack
               direction="row"
               alignItems="center"
