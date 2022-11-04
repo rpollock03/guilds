df6bd04aa3103e511f371de220892237e14ab385
Oliwier Ostrowski <oliwier.ostro@gmail.com>
2022-11-04 17:21:49 +0100

    Assembly the about team leader component

---
+++ components/AboutTheTeamLeader/index.tsx
@@ -0,0 +1,18 @@
+import { Stack } from "@mui/system"
+import { DocumentData } from "firebase/firestore"
+import { Hero } from "storage/hero"
+import { TeamLeaderAvatar } from "./TeamLeaderAvatar"
+import { TeamLeaderInfo } from "./TeamLeaderInfo"
+
+interface AboutTheTeamLeaderProps {
+  teamLeader: DocumentData
+}
+
+export function AboutTheTeamLeader({ teamLeader }: AboutTheTeamLeaderProps) {
+  return (
+    <Stack spacing={3} direction={{ lg: "row", xl: "row" }} alignItems="center">
+      <TeamLeaderInfo teamLeader={teamLeader} />
+      <TeamLeaderAvatar teamLeader={teamLeader} />
+    </Stack>
+  )
+}
