dbf5f5e47ac5058dc96bc78cf23002652ecd1530
Oliwier Ostrowski <oliwier.ostro@gmail.com>
2022-11-06 13:45:13 +0100

    Use stack instead of box in logo component

--- components/Header/Logo.tsx
+++ components/Header/Logo.tsx
@@ -1,21 +1,21 @@
-import { Box, Typography } from "@mui/material"
 import Link from "next/link"
 import Image from "next/image"
+import { Stack, Typography } from "@mui/material"
 
 export const Logo = () => {
   return (
     <Link href="/" style={{ textDecoration: "none", color: "inherit" }}>
-      <Box sx={{ display: "flex", alignItems: "center" }}>
+      <Stack direction="row" alignItems="center" spacing={1}>
         <Image
           src="/guilds-logo.svg"
           width={36}
           height={36}
           alt="Guilds Logo"
         />
-        <Typography variant="h6" sx={{ ml: 0.8, fontWeight: 700 }}>
+        <Typography variant="h6" color="text.primary" fontWeight={700}>
           Guilds
         </Typography>
-      </Box>
+      </Stack>
     </Link>
   )
 }
