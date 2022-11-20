1147cd99f2051293661cc24df72448a4fc876f76
Oliwier Ostrowski <oliwier.ostro@gmail.com>
2022-11-20 16:07:28 +0100

    Add chart title component

--- components/BountiesGraph/Title.tsx
+++ components/BountiesGraph/Title.tsx
@@ -1,51 +1,41 @@
-import { Title } from "@devexpress/dx-react-chart-material-ui"
-import { Typography, IconButton, Stack, Box } from "@mui/material"
+import { Box, IconButton, Stack, Typography } from "@mui/material"
 import { MoreVert } from "@mui/icons-material"
 
-export function CustomTitle({ text, style, ...restProps }) {
+export function ChartTitle() {
   return (
-    <Title.Text
-      text={
-        <Stack>
-          <Stack
-            direction="row"
-            justifyContent="space-between"
-            alignItems="center"
-          >
-            <Typography variant="h5" color="text.primary">
-              {text}
-            </Typography>
-            <IconButton>
-              <MoreVert />
-            </IconButton>
-          </Stack>
-          <Stack direction="row" justifyContent="flex-end" spacing={3}>
-            <Stack direction="row" spacing={1} alignItems="center">
-              <Box
-                width="0.7rem"
-                height="0.7rem"
-                bgcolor="#50915B"
-                borderRadius="50%"
-              />
-              <Typography variant="body2" color="text.secondary">
-                Earnings
-              </Typography>
-            </Stack>
-            <Stack direction="row" spacing={1} alignItems="center">
-              <Box
-                width="0.7rem"
-                height="0.7rem"
-                bgcolor="#7BB785"
-                borderRadius="50%"
-              />
-              <Typography variant="body2" color="text.secondary">
-                XP
-              </Typography>
-            </Stack>
-          </Stack>
+    <Stack spacing={2}>
+      <Stack direction="row" justifyContent="space-between" alignItems="center">
+        <Typography variant="h5" color="text.primary" mb="0">
+          Total Earnings | Total XP Earned
+        </Typography>
+        <IconButton>
+          <MoreVert />
+        </IconButton>
+      </Stack>
+      <Stack direction="row" justifyContent="flex-end" spacing={3}>
+        <Stack direction="row" spacing={1} alignItems="center">
+          <Box
+            width="0.7rem"
+            height="0.7rem"
+            bgcolor="#50915B"
+            borderRadius="50%"
+          />
+          <Typography variant="body2" color="text.secondary">
+            Earnings
+          </Typography>
         </Stack>
-      }
-      {...restProps}
-    />
+        <Stack direction="row" spacing={1} alignItems="center">
+          <Box
+            width="0.7rem"
+            height="0.7rem"
+            bgcolor="#7BB785"
+            borderRadius="50%"
+          />
+          <Typography variant="body2" color="text.secondary">
+            XP
+          </Typography>
+        </Stack>
+      </Stack>
+    </Stack>
   )
 }
