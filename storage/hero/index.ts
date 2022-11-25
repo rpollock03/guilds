26e8f0d9c7c9e6af5c8f8ba45de129f1508113e4
Piotr Ostrowski <piotrostr@google.com>
2022-11-25 19:52:10 +0000

    delete //storage dir altogether

--- storage/hero/index.ts
+++
@@ -1,2 +0,0 @@
-export type { Experience, Hero, Location, Name } from "./types"
-export { populateHeroes } from "./populate"
--- storage/quest/index.ts
+++
@@ -1,2 +0,0 @@
-export { type Quest, Tag, type Bid } from "./types"
-export { populateQuests, populateBids } from "./populate"
