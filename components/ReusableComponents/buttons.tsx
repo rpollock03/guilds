e93074e8f3fdd9eeeedd094df56af7cddb39a4be
Oliwier Ostrowski <oliwier.ostro@gmail.com>
2022-12-01 19:34:16 +0100

    Add background color to white button

--- components/ReusableComponents/buttons.tsx
+++ components/ReusableComponents/buttons.tsx
@@ -24,6 +24,7 @@ export function WhiteButton({ children }) {
         height: "3rem",
         borderRadius: "0.6rem",
         borderColor: (theme) => theme.palette.grey[300],
+        bgcolor: "background.default",
       }}
     >
       <Typography
